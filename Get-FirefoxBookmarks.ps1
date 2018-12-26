d64a204b5802570e24e54f2e8e9dd7f90d6794a6
Robert Hollingshead <robert@roberthollingshead.net>
2018-12-26 14:00:33 -0500

    Commit PS1 File

---
+++ Get-FirefoxBookmarks.ps1
@@ -0,0 +1,124 @@
+# ============================
+#
+# Get-FirefoxBookmarks.ps1
+# by Robert Hollingshead
+#
+# ============================
+
+#Requires -Modules SQLPS
+
+# Location of Bookmark JSON
+$BookmarkPath = 'C:\path\to\your\file.json'
+
+# Location and name of Microsoft SQL Database
+$ServerInstance = 'localhost\SQLExpress'
+$Database = 'PowerShell'
+
+# Convenient function to add string escapes if needed.
+Function Get-StringWithEscape
+{
+    Param($String)
+    Begin{}
+    Process
+    {
+        # Convert to double apostrophe.
+        [string]$EscapedString = $String.replace('''','''''')
+    }
+    End{
+    Return $EscapedString
+    }
+}
+
+# Function to write to the SQL Database
+Function Write-Database
+{
+    Param($BookmarkList,$ServerInstance,$Database)
+    Begin{}
+    Process
+    {
+        Foreach ($Bookmark in $BookmarkList)
+        { 
+            # Add escape characters to strings.
+            [string]$Folder = (Get-StringWithEscape -String $Bookmark.Folder)
+            [string]$Title = (Get-StringWithEscape -String $Bookmark.Title)
+            [string]$URI = (Get-StringWithEscape -String $Bookmark.URI)
+
+            # Create query.
+            $query = "
+                INSERT INTO dbo.FirefoxBookmarks
+                VALUES('$($Folder)',
+                '$($Title)',
+                '$($URI)')
+            "
+
+            # Attempt to execute query. Write out query to host if fails. Useful for debugging.
+            Try { 
+            Invoke-SQLCmd -ServerInstance $ServerInstance -Database $Database -Query $query -ErrorAction Stop
+            }
+            catch {
+                $query
+            }
+        }
+    }
+    End{}
+}
+
+# Get bookmarks from converted object. This is a recursive function. 
+Function Get-Bookmark 
+{
+    Param($BookmarkObject,$ContainerTitle)
+    Begin{
+        [array]$URIList = $null
+    }
+    Process
+    {
+        ForEach ($Child in $BookmarkObject.children) 
+        {
+            switch ($child.typecode) {
+                1 # Typecode 1 means this is a URI.
+                {
+                    $URI = New-Object PSObject
+                    $URI | Add-Member -MemberType NoteProperty -Name Type -Value $($Child.Type)
+                    $URI | Add-Member -MemberType NoteProperty -Name Typecode -Value $($Child.Typecode)
+                    $URI | Add-Member -MemberType NoteProperty -Name Folder -Value $($ContainerTitle)
+                    $URI | Add-Member -MemberType NoteProperty -Name Title -Value $($Child.Title)
+                    $URI | Add-Member -MemberType NoteProperty -Name URI -Value $($Child.URI)
+                    $URIList = $URIList + $URI
+                }
+                2 # Typecode 2 means this is a container of URI.
+                {
+                    # Call function recursively. 
+                    $URIList = $URIList + (Get-Bookmark -BookmarkObject $Child -ContainerTitle $Child.Title)
+                }
+            }
+        }
+    }
+    End
+    {
+        return $URIList
+    }
+}
+
+# Pull in JSON and convert it to PS Object
+$BookmarkFile = Get-Content -path $BookmarkPath
+$Bookmarks = convertfrom-json -InputObject $BookmarkFile
+[array]$BookmarkList = $null
+
+# Set the title of the first container. 
+$ContainerTitle = 'Root'
+
+# See if the FirefoxBookmarks table exists by trying to purge it. If it doesn't exist, create it.
+try 
+{
+    Invoke-SQLCmd -ServerInstance $ServerInstance -Database $Database -Query "DELETE FROM dbo.FirefoxBookmarks" -ErrorAction Stop | out-null
+}
+catch
+{
+    Invoke-SQLCmd -ServerInstance $ServerInstance -Database $Database -Query "BEGIN TRANSACTION GO CREATE TABLE dbo.FirefoxBookmarks (Folder nvarchar(MAX) NULL, Title nvarchar(MAX) NULL, URI nvarchar(MAX) NULL) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY] COMMIT BEGIN TRANSACTION GO ALTER TABLE dbo.FirefoxBookmarks SET (LOCK_ESCALATION = TABLE) COMMIT"
+}
+
+# Create bookmark list.
+$BookmarkList = $BookmarkList + (Get-Bookmark -BookmarkObject $Bookmarks -ContainerTitle $ContainerTitle)
+
+# Write the bookmark list to the database.
+Write-Database -BookmarkList $BookmarkList -ServerInstance $ServerInstance -Database $Database
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # BookmarkJSON2SQL
-Import a Firefox Bookmark JSON to a Microsoft SQL Table
+
+Import a Firefox Bookmark JSON to a Microsoft SQL Table.
+
+## Requirements
+
+* PowerShell Core 6.1
+* The PowerShell SQLPS module loaded using the WindowsCompatibility module.
+* A Microsoft SQL Database
+* An exported Firefox Bookmark .JSON file. 
+
+## Usage
+This is a very basic script for the time being. For now just in an editor like vscode and change the variables to suit your needs. 
+
+This assumes you know how to create and access a Microsoft SQL Database. 
+
+Pay attention to the following variables:
+
+*In this configuration you would be using the PowerShell database on a locally installed instance of MS SQL Express.
+
+```
+# Location of Bookmark JSON
+$BookmarkPath = 'C:\path\to\your\file.json'
+
+# Location and name of Microsoft SQL Database
+$ServerInstance = 'localhost\SQLExpress'
+$Database = 'PowerShell'
+```
+
+## Potential Features in the Future
+
+* URI Valication
+* Store results of HTTP GET in database.
+* Timestamping
